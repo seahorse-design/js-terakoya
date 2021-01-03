a13f9973ce07e69465a4326310712df227102cc4
saki matsushita <saki.seahorse@gmail.com>
2021-01-03 17:43:33 +0900

    01

---
+++ 01/main.js
@@ -0,0 +1,15 @@
+const arrowFunction = () => {
+    return "hello";
+}
+//affowFunctionという変数に、アロー関数で作成した関数を入れる
+
+const res = arrowFunction(); //実行
+console.log(res);
+
+
+//Question
+const uncle = () => {
+    return "Uncle Javascript";
+}
+const display = uncle();
+console.log(display);
